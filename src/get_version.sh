f04c8268cca7abca27508339b08fe3594da0323c
qiaofeng1227 <76487013@qq.com>
2022-07-20 09:27:17 +0800

    Update get_version.sh

--- src/get_version.sh
+++ src/get_version.sh
@@ -1,1 +1,1 @@
-sudo echo "mysql version:" $(docker inspect nextcloud-db |grep -i mysql_version |cut -d= -f2) |sudo tee -a /data/logs/install_version.txt
+sudo echo "nextcloud version:" $(docker exec -i $1 cat version.php |grep OC_VersionString |awk -F "'" '{print $2}') |sudo tee -a /data/logs/install_version.txt
